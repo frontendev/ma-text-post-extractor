41e7478734b30ef0246bba85c9291db2003faa15
Damian <d.ezequiel.palacios@gmail.com>
2022-10-12 09:41:28 -0300

    Update format. Add new tags. Remove signature

--- getInfo.js
+++ getInfo.js
@@ -1,41 +1,46 @@
 const getBandInfo = (songNumber, country, demonym, subgenre, profile) => {
-    const clearDate = (rDate) => {
-      const date = rDate.toString();
-      // Just year
-      if (date.length === 4) {
+
+  // To match the same format as Bandcamp
+  const clearDate = (rDate) => {
+    const date = rDate.toString();
+    // Just year
+    if (date.length === 4) {
+      return date;
+    } else if (date.match(',')) {
+        const start = date.match(' ');
+        const end = date.match(',');
+        const suffix = date.substring(end.index - 2, end.index)
+        // search only in the second half to avoid removing chars from Month names
+        const removeSuffix = date.substring(start.index).replace(suffix, '');
+        return `${date.substring(0, start.index)} ${removeSuffix}`;
+      } else {
         return date;
-      } else if (date.match(',')) {
-          const start = date.match(' ');
-          const end = date.match(',');
-          const suffix = date.substring(end.index - 2, end.index)
-          // search only in the second half to avoid removing chars from Month names
-          const removeSuffix = date.substring(start.index).replace(suffix, '');
-          return `${date.substring(0, start.index)} ${removeSuffix}`;
-        } else {
-          return date;
-        }
-    };
-    const info = {
-        'Band': document.querySelector(".band_name").innerText,
-        'Song': document.querySelectorAll('.wrapWords')[(songNumber - 1)].innerText,
-        'Album': document.querySelector(".album_name a").innerText,
-        'Released': clearDate(document.querySelector("#album_info > .float_left > dd:nth-child(4)").innerText),
-        'Label': document.querySelector("#album_info > .float_right > dd:nth-child(2)").innerText,
-        'Country': country,
-        'Subgenre': subgenre,
-    }
+      }
+  };
+
+  // Build an object with keys and values
+  const info = {
+      'Band': document.querySelector(".band_name").innerText,
+      'Song': document.querySelectorAll('.wrapWords')[(songNumber - 1)].innerText,
+      'Type': document.querySelector("#album_info > dl.float_left > dd:nth-child(2)").innerText,
+      'Name': document.querySelector(".album_name a").innerText,
+      'Released': clearDate(document.querySelector("#album_info > .float_left > dd:nth-child(4)").innerText),
+      'Label': document.querySelector("#album_info > .float_right > dd:nth-child(2)").innerText,
+      'Country': country,
+      'Subgenre': subgenre,
+  }
+
   // Reduce will iterate over all the array items and returns a single value.
   const listItems = Object.entries(info).reduce((result, item) => {
-    // Add a string to the result for the current item. This syntax is using template literals.
-    result += `<li>${item[0]}: ${item[1]}</li>`;
-    // Always return the result in the reduce callback, it will be the value or result in the next iteration.
+    result += (item[1] && `<li>${item[0]}: ${item[1]}</li>`);
     return result;
-  }, ''); // The '' is an empty string, it is the initial value result.
-  // Get the element from the DOM in which to display the list, this should be an ul or ol element.
+  }, '');
+  // Get the element from the DOM in which to display the list
   resultElement = document.getElementById('message');
   // Set the inner HTML
   resultElement.innerHTML = listItems;
-  // Add Line up section
+
+  // Line up section
   const losotro = Array.from(document.querySelectorAll("#album_members_lineup .ui-tabs-panel-content .lineupRow"))
   if (losotro.length > 0) {
     const elarray = losotro.map(
@@ -48,6 +53,8 @@ const getBandInfo = (songNumber, country, demonym, subgenre, profile) => {
     resultElement.insertAdjacentHTML("beforeend", "Line up:");
     resultElement.insertAdjacentHTML("beforeend", elarray.join(""));
   }
+
+  // Creating tags format
   const taggify = (words) => {
     // Split each string into separated words to
     // clean unwanted characters and then join all
@@ -73,6 +80,7 @@ const getBandInfo = (songNumber, country, demonym, subgenre, profile) => {
       }).join("").toLowerCase();
       return `#${cleanString}`;
   }
+
   // Tag cloud creation
   const createTags = (bandMembers) => {
     let allTags = [
@@ -82,8 +90,8 @@ const getBandInfo = (songNumber, country, demonym, subgenre, profile) => {
       `${taggify(info.Band)}band`,
       `${taggify(info.Band)}blackmetal`,
       `${taggify(info.Band)}bm`,
-      info.Song === info.Album ? '' : taggify(info.Album),
-      taggify(info.Label)
+      info.Song === info.Name ? '' : taggify(info.Name),
+      info.Label === "Independent" ? '#independentrelease' : taggify(info.Label)
     ];
     if (bandMembers.length > 0) {
       const names = bandMembers.map(
@@ -93,15 +101,23 @@ const getBandInfo = (songNumber, country, demonym, subgenre, profile) => {
         }
       );
     }
-    allTags.push(taggify(`${demonym}blackmetal`), taggify(`${demonym}metal`));
-    allTags.push(taggify(subgenre))
+    allTags.push(taggify(`${demonym}blackmetal`), taggify(`${demonym}metal`), taggify(`${demonym}metalband`), taggify(`${demonym}bm`) );
+    if (subgenre) {
+      allTags.push(taggify(subgenre))
+    }
+    allTags.push(taggify("blackmetalalbum"))
+    allTags.push(taggify("blackmetalband"))
     allTags.push(taggify("blackmetaldaily"))
     return allTags.join(" ");
   }
+
+  // If it is available, add Instagram mention
   if (profile.length > 0) {
-    resultElement.insertAdjacentHTML("beforeend", `<p>${profile}</p>`);
+    resultElement.insertAdjacentHTML("beforeend", `<p>@${profile}</p>`);
   }
-  resultElement.insertAdjacentHTML("beforeend", `<p>HH</p>`);
+
+  resultElement.insertAdjacentHTML("beforeend", `<p></p>`);
   resultElement.insertAdjacentHTML("beforeend", createTags(losotro));
 }
-getBandInfo(1, 'Country', 'demonym', 'subgenre', '@instagram-id');
+
+getBandInfo(6, 'Norway', 'Norwegian', 'Depressive Black Metal', '');
